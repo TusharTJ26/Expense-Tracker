297bcc468cc38e3d3d5a245541d8f8983189797a
Tushar Jain <tushar.26tj@gmail.com>
2025-11-14 13:53:07 +0530

    Changes5

--- src/Resent-Transaction/ResentTransaction.js
+++ src/Resent-Transaction/ResentTransaction.js
@@ -187,7 +187,11 @@ export default function ResentTransaction({
       style={{ backgroundColor: "white", border: "none", borderRadius: "1rem" }}
     >
       {currentItems.map((item, index) => (
-        <div key={indexOfFirstItem + index} className="card-holder">
+        <div
+          key={indexOfFirstItem + index}
+          className="card-holder Transactions"
+          name="Transactions"
+        >
           <div className="nameIcon">
             <div className="icon">
               {categoryIcon(item.category)}
